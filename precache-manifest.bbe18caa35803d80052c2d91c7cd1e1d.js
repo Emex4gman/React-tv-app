1f32c77e5df337ec0f297ee2ebbc7eee528b6349
EMEX <emex4gman@gmail.com>
2019-09-22 04:01:23 +0300

    Updates

---
+++ precache-manifest.bbe18caa35803d80052c2d91c7cd1e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636e3bbb9a4edb210f1915186103b3e6",
+    "url": "/React-tv-app/index.html"
+  },
+  {
+    "revision": "59f1d4d297850e5e6c51",
+    "url": "/React-tv-app/static/css/main.88a7ba1c.chunk.css"
+  },
+  {
+    "revision": "f7f19845744c92321a13",
+    "url": "/React-tv-app/static/js/2.409cc7f3.chunk.js"
+  },
+  {
+    "revision": "59f1d4d297850e5e6c51",
+    "url": "/React-tv-app/static/js/main.3736c722.chunk.js"
+  },
+  {
+    "revision": "30a994d15324f6b3c9d8",
+    "url": "/React-tv-app/static/js/runtime~main.d526bf16.js"
+  },
+  {
+    "revision": "ad1152b599989b3f1f49733bcf63a18e",
+    "url": "/React-tv-app/static/media/loader.ad1152b5.gif"
+  }
+]);
