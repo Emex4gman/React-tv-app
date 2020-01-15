e8cc8b2e297de94d65c55e6a8fc0768e062f5999
emex4gman <emex4gman@gmail.com>
2020-01-15 23:21:42 +0100

    update

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ import { BrowserRouter } from 'react-router-dom';
 //const greeting = React.createElement('h1', {}, 'hello world');
 
 ReactDOM.render(
- <BrowserRouter><App /></BrowserRouter>,
- document.getElementById('root')
- );
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
